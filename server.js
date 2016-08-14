6e1d0a0a79afb819cd9a4cb6279302b20da2e5cc
CuongDao <dqcuong93@gmail.com>
2016-08-14 21:38:03 +0700

    Add POST fot todos

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 var express = require('express');
+var bodyParse = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'go buy some food',
-    completed: false
-}, {
-    id: 2,
-    description: 'go to the movie theater',
-    completed: false
-}, {
-    id: 3,
-    description: 'eat dinner with family',
-    completed: true
-}];
+//var todos = [{
+//    id: 1,
+//    description: 'go buy some food',
+//    completed: false
+//}, {
+//    id: 2,
+//    description: 'go to the movie theater',
+//    completed: false
+//}, {
+//    id: 3,
+//    description: 'eat dinner with family',
+//    completed: true
+//}];
+var todos = [];
+var todoNextID = 1;
+
+app.use(bodyParse.json());
 
 app.get('/', function (req, res) {
     res.send('Todo api root');
@@ -40,6 +45,15 @@ app.get('/todos/:id', function (req, res) {
     } else {
         res.status(404).send();
     }
+});
+
+//POST todos/ with body-parser module
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    body.id = todoNextID;
+    todos.push(body);
+    todoNextID += 1;
+    res.send(body);
 });
 
 app.listen(PORT, function () {
