9daa80bcdd5317df4720d5d91313e7b940693a0e
CuongDao <dqcuong93@gmail.com>
2016-09-19 16:05:55 +0700

    Hash and salt password

--- server.js
+++ server.js
@@ -225,12 +225,11 @@ app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function (user) {
-        res.status(200).json(user.toJSON());
+        res.status(200).json(user.toPublicJSON());
     }, function (e) {
         res.status(400).json(e);
     })
 });
-
 
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
